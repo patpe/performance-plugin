402ff92be7d1ac4fbc720f810244321afd0e19d2
Patrik Persson <patrikp@gmail.com>
2014-12-23 17:32:55 +0100

    Improving code quality according to sonar

--- src/main/java/se/raketavdelningen/ci/jenkins/performance/PerformanceReportConstants.java
+++ src/main/java/se/raketavdelningen/ci/jenkins/performance/PerformanceReportConstants.java
@@ -3,9 +3,9 @@ package se.raketavdelningen.ci.jenkins.performance;
 public interface PerformanceReportConstants {
 
 	public static final String PLUGIN_NAME = "Performance Report";
-	
+
 	public static final String PLUGIN_URL = "performance";
-	
+
 	public static final String PLUGIN_ICON = "performance.gif";
-	
+
 }
--- src/main/java/se/raketavdelningen/ci/jenkins/performance/aggregator/TimeBasedAggregator.java
+++ src/main/java/se/raketavdelningen/ci/jenkins/performance/aggregator/TimeBasedAggregator.java
@@ -13,79 +13,79 @@ import hudson.model.Descriptor;
  */
 public class TimeBasedAggregator extends Aggregator {
 
-    private int minutesPerAggregation = 1;
-    
-    private long nextTimestamp = -1;
-    
-    @Override
-    public boolean isSampleInCurrentAggregation(PerformanceSample sample) {
-        return (sample.getTimestamp() <= nextTimestamp);
-    }
+	private int minutesPerAggregation = 1;
 
-    @Override
-    public AggregatedPerformanceSample aggregatePerformanceSamples(
-            List<PerformanceSample> samples, String key) {
-        long aggregatedTimestamp = calculateAggregatedTimestamp(samples);
-        long samplesCounter = 0;
-        long sumOfElapsed = 0;
-        boolean success = true;
-        
-        long max = Long.MIN_VALUE;
-        long min = Long.MAX_VALUE;
-        for (PerformanceSample sample : samples) {
-            long elapsed = sample.getElapsed();
-            if (max < elapsed) {
-                max = elapsed;
-            }
-            if (elapsed < min) {
-                min = elapsed;
-            }
-            
-            sumOfElapsed+=sample.getElapsed();
-            success = (success && sample.isSuccess());
-            samplesCounter++;
-        }
-        
-        long averageElapsed = sumOfElapsed / samplesCounter;
-        
-        return new AggregatedPerformanceSample(aggregatedTimestamp, max, min, averageElapsed, 
-                success, samplesCounter, key);
-    }
+	private long nextTimestamp = -1;
 
-    private long calculateAggregatedTimestamp(List<PerformanceSample> samples) {
-        if (samples.size() > 1) {
-	    	long firstTimestamp = samples.get(0).getTimestamp();
-	        long lastTimestamp = samples.get(samples.size() - 1).getTimestamp();
-	        return (lastTimestamp + firstTimestamp) / 2;
-        } else if (samples.size() == 1) {
-        	return samples.get(0).getTimestamp();
-        } else {
-        	throw new PerformanceReportException("No samples given to analyze");
-        }
-    }
+	@Override
+	public boolean isSampleInCurrentAggregation(PerformanceSample sample) {
+		return sample.getTimestamp() <= nextTimestamp;
+	}
 
-    @Override
-    public void startNewAggregationPeriod() {
-        this.nextTimestamp = calculateNextTimestamp(nextTimestamp);
-    }
-    
-    @Override
-    public void initializeAggregatorFromFirstSample(PerformanceSample firstSample) {
-        this.nextTimestamp = calculateNextTimestamp(firstSample.getTimestamp());
-    }
+	@Override
+	public AggregatedPerformanceSample aggregatePerformanceSamples(
+			List<PerformanceSample> samples, String key) {
+		long aggregatedTimestamp = calculateAggregatedTimestamp(samples);
+		long samplesCounter = 0;
+		long sumOfElapsed = 0;
+		boolean success = true;
 
-    @Override
-    public Descriptor<Aggregator> getDescriptor() {
-        return new Descriptor<Aggregator>() {    
-        
-            @Override
-            public String getDisplayName() {
-                return "Time based aggregator (1 min interval)";
-            }
-        };
-    }
-    
-    private long calculateNextTimestamp(long currentNextTimestamp) {
-        return currentNextTimestamp + (minutesPerAggregation * 60 * 1000);
-    }
+		long max = Long.MIN_VALUE;
+		long min = Long.MAX_VALUE;
+		for (PerformanceSample sample : samples) {
+			long elapsed = sample.getElapsed();
+			if (max < elapsed) {
+				max = elapsed;
+			}
+			if (elapsed < min) {
+				min = elapsed;
+			}
+
+			sumOfElapsed+=sample.getElapsed();
+			success = (success && sample.isSuccess());
+			samplesCounter++;
+		}
+
+		long averageElapsed = sumOfElapsed / samplesCounter;
+
+		return new AggregatedPerformanceSample(aggregatedTimestamp, max, min, averageElapsed, 
+				success, samplesCounter, key);
+	}
+
+	private long calculateAggregatedTimestamp(List<PerformanceSample> samples) {
+		if (samples.size() > 1) {
+			long firstTimestamp = samples.get(0).getTimestamp();
+			long lastTimestamp = samples.get(samples.size() - 1).getTimestamp();
+			return (lastTimestamp + firstTimestamp) / 2;
+		} else if (samples.size() == 1) {
+			return samples.get(0).getTimestamp();
+		} else {
+			throw new PerformanceReportException("No samples given to analyze");
+		}
+	}
+
+	@Override
+	public void startNewAggregationPeriod() {
+		this.nextTimestamp = calculateNextTimestamp(nextTimestamp);
+	}
+
+	@Override
+	public void initializeAggregatorFromFirstSample(PerformanceSample firstSample) {
+		this.nextTimestamp = calculateNextTimestamp(firstSample.getTimestamp());
+	}
+
+	@Override
+	public Descriptor<Aggregator> getDescriptor() {
+		return new Descriptor<Aggregator>() {    
+
+			@Override
+			public String getDisplayName() {
+				return "Time based aggregator (1 min interval)";
+			}
+		};
+	}
+
+	private long calculateNextTimestamp(long currentNextTimestamp) {
+		return currentNextTimestamp + (minutesPerAggregation * 60 * 1000);
+	}
 }
